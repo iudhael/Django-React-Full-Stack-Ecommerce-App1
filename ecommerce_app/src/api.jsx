6589b60afa4d4e8b723ec576b1cdb16ed9255d95
iudhael <adikpetoiudhael@gmail.com>
2025-03-02 21:32:20 +0100

    ajout de apiUrl dans le fichier api.jsx de  ecommerce_app

--- ecommerce_app/src/api.jsx
+++ ecommerce_app/src/api.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+
+const apiUrl = "/choreo-apis/ecommerceapp1/backend/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
   })
   
 export default api
